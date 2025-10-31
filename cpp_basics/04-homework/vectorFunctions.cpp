c30b3562ff2c8d3a29618f7fc6209a9f2d813f56
Adam Prystupa <prystupaadam@gmail.com>
2025-10-31 16:54:56 +0100

    cpp_basics finished

--- cpp_basics/04-homework/vectorFunctions.cpp
+++ cpp_basics/04-homework/vectorFunctions.cpp
@@ -1,1 +1,31 @@
 #include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(const int &count) {
+  std::vector<std::shared_ptr<int>> vec;
+  for (size_t i = 0; i < count; ++i) {
+    std::shared_ptr<int> temp = std::make_shared<int>(i);
+    vec.push_back(temp);
+  }
+  return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>> &vec) {
+  for (auto element : vec) {
+    std::cout << *element << ", ";
+  }
+  std::cout << "\n";
+}
+
+void add10(std::vector<std::shared_ptr<int>> &vec) {
+  for (auto element : vec) {
+    *element += 10;
+  }
+}
+
+void sub10(int *const ptr) { *ptr -= 10; }
+
+void sub10(std::vector<std::shared_ptr<int>> &vec) {
+  for (auto element : vec) {
+    sub10(element.get());
+  }
+}
--- cpp_basics/04-homework/vectorFunctions.hpp
+++ cpp_basics/04-homework/vectorFunctions.hpp
@@ -1,10 +1,10 @@
-#ifndef VECTORFUNCTIONS.HPP
-#define VECTORFUNCTIONS .HPP
+#ifndef VECTORFUNCTIONS_HPP
+#define VECTORFUNCTIONS_HPP
 
+#include <iostream>
 #include <memory>
 #include <vector>
-
-std::vector<std::shared_ptr<int>> generate(int &const);
+std::vector<std::shared_ptr<int>> generate(const int &);
 void print(std::vector<std::shared_ptr<int>> &);
 void add10(std::vector<std::shared_ptr<int>> &);
 void sub10(int *const);
